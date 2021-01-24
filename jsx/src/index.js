f5d2fab509807577d5640897370b3af1394790d8
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-01-24 21:33:37 +0700

    [ADD] JSX section

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Import React and ReactDOM libaries
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Create a React component
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  const buttonText = 'Click me'
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter Name
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText}</button>
+    </div>
+  )
+}
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.getElementById('root'))
