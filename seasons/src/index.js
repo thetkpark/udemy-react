a523edc30b588c0547fab2b708d2770920a784ae
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-01-25 15:16:32 +0700

    [ADD] Seasons with class component

---
+++ seasons/src/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// const App = () => {
+// 	window.navigator.geolocation.getCurrentPosition(
+// 		position => console.log(position),
+// 		err => console.log(err)
+// 	)
+
+// 	return <div>Hi There</div>
+// }
+
+class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = { lat: null, errorMessage: '' }
+
+		window.navigator.geolocation.getCurrentPosition(
+			position => {
+				this.setState({ lat: position.coords.latitude })
+			},
+			err => this.setState({ errorMessage: err.message })
+		)
+	}
+
+	render() {
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div>Error: {this.state.errorMessage}</div>
+		} else if (!this.state.errorMessage && this.state.lat) {
+			return <div>Latitude: {this.state.lat}</div>
+		}
+		return <div>Loading!</div>
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
