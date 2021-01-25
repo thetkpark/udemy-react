912469eff74bca867fcee6da8c69b134a585fe43
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-01-25 17:22:29 +0700

    [ADD] componentDidMount

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -11,17 +11,7 @@ import ReactDOM from 'react-dom'
 // }
 
 class App extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = { lat: null, errorMessage: '' }
-
-		window.navigator.geolocation.getCurrentPosition(
-			position => {
-				this.setState({ lat: position.coords.latitude })
-			},
-			err => this.setState({ errorMessage: err.message })
-		)
-	}
+	state = { lat: null, errorMessage: '' }
 
 	render() {
 		if (this.state.errorMessage && !this.state.lat) {
@@ -30,6 +20,13 @@ class App extends React.Component {
 			return <div>Latitude: {this.state.lat}</div>
 		}
 		return <div>Loading!</div>
+	}
+
+	componentDidMount() {
+		window.navigator.geolocation.getCurrentPosition(
+			position => this.setState({ lat: position.coords.latitude }),
+			err => this.setState({ errorMessage: err.message })
+		)
 	}
 }
 
