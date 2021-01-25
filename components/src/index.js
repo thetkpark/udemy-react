dfa25e9682280580b0acb9f928ef1f00cf5002c6
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-01-25 08:07:58 +0700

    [FINISH] props section

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import faker from 'faker'
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Todat at 4:45 PM"
-        content="Nice"
-        avatar={faker.image.image()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Todat at 2:00 PM"
-        content="Very Good"
-        avatar={faker.image.image()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 5:00 PM"
-        content="What's is this"
-        avatar={faker.image.image()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Todat at 4:45 PM"
+          content="Nice"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Todat at 2:00 PM"
+          content="Very Good"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00 PM"
+          content="What's is this"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   )
 }
