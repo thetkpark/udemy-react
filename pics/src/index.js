d30ae24e597eeb4514729b1d25d9d214200061c8
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-01-26 20:53:01 +0700

    [ADD] Axios to unsplash

--- pics/src/index.js
+++ pics/src/index.js
@@ -2,4 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
+// console.log(process.env.REACT_APP_UNSPLASH_ACCESS_KEY)
+
 ReactDOM.render(<App />, document.querySelector('#root'))
