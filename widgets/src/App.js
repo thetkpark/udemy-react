6938ffb2a8d051237c943c26baaeff249ea23935
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-02-22 21:50:22 +0700

    [ADD] init translate

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Accordion from './components/Accordion'
 import Search from './components/Search'
 import Dropdown from './components/Dropdown'
+import Translate from './components/Translate'
 
 const items = [
   {
@@ -35,16 +36,13 @@ const options = [
 
 const App = () => {
   const [selected, setSelected] = useState(options[0])
-  const [showDropdown, setShowDropdown] = useState(true)
 
   return (
     <div>
       {/* <Accordion items={items} /> */}
       {/* <Search /> */}
-      <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
-      {showDropdown ? (
-        <Dropdown options={options} selected={selected} onSelectedChange={setSelected} />
-      ) : null}
+      {/* <Dropdown options={options} selected={selected} onSelectedChange={setSelected} /> */}
+      <Translate />
     </div>
   )
 }
