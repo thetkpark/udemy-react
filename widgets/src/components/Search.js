28dffc9772ea0e260feaeaeb0887bbdf76ea9aca
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-02-22 14:15:59 +0700

    [ADD] Wikipedia API with useEffect

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,14 +1,48 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Search = () => {
-  const [term, setTerm] = useState('')
+  const [term, setTerm] = useState('programming')
+  const [results, setResults] = useState([])
 
   useEffect(() => {
-    console.log('I Only run once')
-  }, [])
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term
+        }
+      })
+      setResults(data.query.search)
+    }
+
+    search()
+  }, [term])
   // [] -> Run at initial render only
   // no args -> + run after every rerender
   // [vars] -> + run after every rerender if data has changed
+
+  const rederedResults = results.map(result => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    )
+  })
 
   return (
     <div>
@@ -18,6 +52,7 @@ const Search = () => {
           <input className="input" value={term} onChange={e => setTerm(e.target.value)} />
         </div>
       </div>
+      <div className="ui celled list">{rederedResults}</div>
     </div>
   )
 }
