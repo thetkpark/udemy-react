1b41509eadcd754e8ab3feafc335f67aaacac661
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-02-24 13:40:19 +0700

    Route by Route conponent

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -3,6 +3,7 @@ import Accordion from './components/Accordion'
 import Search from './components/Search'
 import Dropdown from './components/Dropdown'
 import Translate from './components/Translate'
+import Route from './components/Route'
 
 const items = [
   {
@@ -39,10 +40,23 @@ const App = () => {
 
   return (
     <div>
-      {/* <Accordion items={items} /> */}
-      {/* <Search /> */}
-      {/* <Dropdown options={options} selected={selected} onSelectedChange={setSelected} /> */}
-      <Translate />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   )
 }
