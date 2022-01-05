fdc0e91832803581769a98c95c479cd8930d78eb
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-01-05 20:08:10 +0700

    wired up redux-thunk

---
+++ blog/src/actions/index.js
@@ -0,0 +1,11 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder'
+
+export const fetchPosts = () => async dispatch => {
+  const res = await jsonPlaceholder.get('/posts')
+
+  // need to call dispatch manually with action
+  dispatch({
+    type: 'FETCH_POSTS',
+    payload: res
+  })
+}
