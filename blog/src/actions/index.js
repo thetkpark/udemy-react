f45c0c75f05f1adc403e85dfdfe378c54ccbefe3
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-01-05 22:53:46 +0700

    use lodash memoize to reduce fetching

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
 export const fetchPosts = () => async dispatch => {
@@ -10,11 +11,14 @@ export const fetchPosts = () => async dispatch => {
   })
 }
 
-export const fetchUser = id => async dispatch => {
-  const res = await jsonPlaceholder.get(`/users/${id}`)
+export const fetchUser = id => dispatch => {
+  _fetchUser(id, dispatch)
+}
 
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const res = await jsonPlaceholder.get(`/users/${id}`)
   dispatch({
     type: 'FETCH_USER',
     payload: res.data
   })
-}
+})
