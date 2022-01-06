1ad210a2c60875dd6c7e0a4e9252bddfc3bd65b2
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-01-06 15:28:03 +0700

    add routes

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import StreamCreate from "./streams/StreamCreate"
+import StreamEdit from "./streams/StreamEdit"
+import StreamDelete from "./streams/StreamDelete"
+import StreamList from "./streams/StreamList"
+import StreamShow from "./streams/StreamShow"
+
 const App = () => {
-	return <div>App</div>
+	return (
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<StreamList />} />
+					<Route path="/streams/new" element={<StreamCreate />} />
+					<Route path="/streams/edit" element={<StreamEdit />} />
+					<Route path="/streams/delete" element={<StreamDelete />} />
+					<Route path="/streams/show" element={<StreamShow />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	)
 }
 
 export default App
--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -1,17 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
 import App from "./components/App"
 
-const PageOne = () => <div>Page One</div>
-const PageTwo = () => <div>Page Two</div>
-
-ReactDOM.render(
-	<BrowserRouter>
-		<Routes>
-			<Route path="/" element={<PageOne />} />
-			<Route path="/pagetwo" element={<PageTwo />} />
-		</Routes>
-	</BrowserRouter>,
-	document.getElementById("root")
-)
+ReactDOM.render(<App />, document.getElementById("root"))
