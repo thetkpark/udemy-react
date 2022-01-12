4ab443075122c98da41d2611b5a1c19ba8421bed
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-01-12 13:23:34 +0700

    use context in class components

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,5 +1,23 @@
+import { useState } from 'react'
+import UserCreate from './UserCreate'
+
 const App = () => {
-	return <div>App</div>
+  const [language, setLanguage] = useState('english')
+
+  const onLanguageChange = lang => {
+    setLanguage(lang)
+  }
+
+  return (
+    <div className="ui container">
+      <div>
+        Select a language
+        <i className="flag us" onClick={() => onLanguageChange('english')} />
+        <i className="flag nl" onClick={() => onLanguageChange('dutch')} />
+      </div>
+      <UserCreate />
+    </div>
+  )
 }
 
 export default App
